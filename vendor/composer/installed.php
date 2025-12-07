499e3f1b1397ccb2705f9a9280412c271e7ce13b
julianne-glitch <141750012+julianne-glitch@users.noreply.github.com>
2025-12-07 15:10:26 +0100

    Install PHPMailer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'xafpay/backend',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '149a53f294b97c6f2a984dd797351abd6e88d247',
+        'reference' => '9e6eebdbfdd1666be3a44dcd029104ffb18ef009',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -157,7 +157,7 @@
         'xafpay/backend' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '149a53f294b97c6f2a984dd797351abd6e88d247',
+            'reference' => '9e6eebdbfdd1666be3a44dcd029104ffb18ef009',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
