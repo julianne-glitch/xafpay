ea9cc6b4032495922e10310f54f51336910cb2e6
julianne-glitch <141750012+julianne-glitch@users.noreply.github.com>
2025-12-08 17:58:44 +0100

    Fix mailer.php remove deprecated properties

--- api/mailer.php
+++ api/mailer.php
@@ -12,31 +12,24 @@ function smtp_send($to, $subject, $html, $cfg)
     $mail = new PHPMailer();
 
     try {
-
-        // FORCE SMTP ONLY
-        $mail->IsSMTP();       
+        $mail->IsSMTP();
         $mail->SMTPAuth = true;
-        
-        // BASIC SMTP SETTINGS (old PHPMailer compatible)
-        $mail->Host = $cfg['host'];
-        $mail->Port = (int)$cfg['port'];
+
+        $mail->Host     = $cfg['host'];
+        $mail->Port     = (int)$cfg['port'];
         $mail->Username = $cfg['username'];
         $mail->Password = $cfg['password'];
-        $mail->SMTPSecure = "tls";  // old PHPMailer supports this
-       
-        // Disable sendmail fallback completely
+        $mail->SMTPSecure = "tls";
+
         $mail->Mailer = "smtp";
 
-        // FROM & TO
         $mail->SetFrom($cfg['from_email'], $cfg['from_name']);
         $mail->AddAddress($to);
 
-        // EMAIL CONTENT
         $mail->IsHTML(true);
         $mail->Subject = $subject;
-        $mail->Body = $html;
+        $mail->Body    = $html;
 
-        // SEND
         if (!$mail->Send()) {
             error_log("SMTP ERROR: " . $mail->ErrorInfo);
             return false;
@@ -49,3 +42,4 @@ function smtp_send($to, $subject, $html, $cfg)
         return false;
     }
 }
+
