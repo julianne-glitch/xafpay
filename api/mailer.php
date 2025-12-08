a6da5c8234434a98d53d5abbf37734f480e7ffec
julianne-glitch <141750012+julianne-glitch@users.noreply.github.com>
2025-12-08 10:12:11 +0100

    MailerSend SMTP update + webhook email sending fix

--- api/mailer.php
+++ api/mailer.php
@@ -2,7 +2,6 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-// Include PHPMailer manually
 require_once __DIR__ . '/phpmailer/PHPMailer.php';
 require_once __DIR__ . '/phpmailer/SMTP.php';
 require_once __DIR__ . '/phpmailer/Exception.php';
@@ -14,44 +13,30 @@ function smtp_send($to, $subject, $html, $cfg)
     $mail = new PHPMailer(true);
 
     try {
-        // ---------------------------------------------------
-        // SMTP MODE FIRST
-        // ---------------------------------------------------
+        // SMTP CONFIG
         $mail->isSMTP();
-
-        // ---------------------------------------------------
-        // DEBUG (prints to Render logs)
-        // ---------------------------------------------------
-        $mail->SMTPDebug = 2;
-        $mail->Debugoutput = 'error_log';
-
-        // ---------------------------------------------------
-        // SMTP SETTINGS
-        // ---------------------------------------------------
-        $mail->Host       = $cfg['host'];      
+        $mail->Host       = $cfg['host'];
         $mail->SMTPAuth   = true;
-        $mail->Username   = $cfg['username'];  
-        $mail->Password   = $cfg['password'];  
-        $mail->SMTPSecure = 'tls';             
-        $mail->Port       = $cfg['port'];      
+        $mail->Username   = $cfg['username'];
+        $mail->Password   = $cfg['password'];
 
-        // ---------------------------------------------------
-        // FROM / TO
-        // ---------------------------------------------------
+        // WORKING SMTP DEBUG
+        $mail->SMTPDebug = 2;
+        $mail->Debugoutput = function($msg) {
+            error_log("SMTP-DEBUG: " . $msg);
+        };
+
+        // TLS encryption
+        $mail->SMTPSecure = 'tls';
+        $mail->Port       = $cfg['port'];
+
         $mail->setFrom($cfg['from_email'], $cfg['from_name']);
         $mail->addAddress($to);
 
-        // ---------------------------------------------------
-        // CONTENT
-        // ---------------------------------------------------
         $mail->isHTML(true);
-        $mail->CharSet = 'UTF-8';     // ⭐ IMPORTANT FIX
         $mail->Subject = $subject;
         $mail->Body    = $html;
 
-        // ---------------------------------------------------
-        // SEND
-        // ---------------------------------------------------
         $mail->send();
         return true;
 
